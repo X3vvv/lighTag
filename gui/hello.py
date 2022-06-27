0ccd36dbca36c0c982f2b728547d9d79791c3442
Zewen PAN <65343237+X3vvv@users.noreply.github.com>
2022-06-28 02:59:07 +0800

    box layout
    use BoxLayout with:
    - size_hint
    - pos_hint
    - width / height

--- gui/hello.py
+++ gui/hello.py
@@ -8,32 +8,13 @@ from kivy.lang import Builder
 Builder.load_file("hello.kv")  # explicitly assign the kv file with random name
 
 
-class MyGridLayout(GridLayout):
-
-    name = ObjectProperty(None)
-    pwd = ObjectProperty(None)
-
-    def press(self):
-        # Function of the submit button
-        name = self.name.text
-        pwd = self.pwd.text
-
-        # print("Hello {}, your password is: {}".format(name, pwd))
-
-        # Print info to screen
-        if name.strip() == "" or pwd.strip() == "":
-            msg = "Error: must fill the username and password"
-        else:
-            msg = "Hello {}, your password is: {}".format(name, pwd)
-            # Clear the input boxes
-            self.name.text = ""
-            self.pwd.text = ""
-        self.add_widget(Label(text=msg))
+class MyLayout(GridLayout):
+    pass
 
 
 class AwesomeApp(App):
     def build(self):
-        return MyGridLayout()
+        return MyLayout()
 
 
 if __name__ == "__main__":

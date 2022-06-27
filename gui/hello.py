34511660abbd488fbd4a08b6fb121a761ee72b4e
Zewen PAN <65343237+X3vvv@users.noreply.github.com>
2022-06-28 02:30:53 +0800

    Restructure & simplify hello.py by using kv file.

--- gui/hello.py
+++ gui/hello.py
@@ -1,45 +1,16 @@
-import kivy
 from kivy.app import App
 
 from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
-from kivy.uix.textinput import TextInput
-from kivy.uix.button import Button
+from kivy.properties import ObjectProperty
 
 
 class MyGridLayout(GridLayout):
-    # Initialize infinite keywords
-    def __init__(self, **kwargs):
-        # Call grid layout constructor
-        super(MyGridLayout, self).__init__(**kwargs)
 
-        # Set columns
-        self.cols = 1
+    name = ObjectProperty(None)
+    pwd = ObjectProperty(None)
 
-        # Create a second grid layout
-        self.top_grid = GridLayout()
-        self.top_grid.cols = 2
-
-        # Add the second grid layout to the screen
-        self.add_widget(self.top_grid)
-
-        # Add username input box
-        self.top_grid.add_widget(Label(text="Username: "))
-        self.name = TextInput(multiline=False)
-        self.top_grid.add_widget(self.name)
-
-        # Add password input box
-        self.top_grid.add_widget(Label(text="Password: "))
-        self.pwd = TextInput(multiline=False, password=True)
-        self.top_grid.add_widget(self.pwd)
-
-        # Create submit button
-        self.submit = Button(text="Submit", font_size=32)
-        # Bind the button
-        self.submit.bind(on_press=self.press)
-        self.add_widget(self.submit)
-
-    def press(self, instance):
+    def press(self):
         # Function of the submit button
         name = self.name.text
         pwd = self.pwd.text

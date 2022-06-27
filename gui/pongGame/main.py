08a901e01094c6cab38777887c7c3a151c283055
Zewen PAN <65343237+X3vvv@users.noreply.github.com>
2022-06-27 12:03:05 +0800

    finish pong game

--- gui/pongGame/main.py
+++ gui/pongGame/main.py
@@ -7,6 +7,21 @@ from kivy.clock import Clock
 from random import randint
 
 
+class PongPaddle(Widget):
+
+    score = NumericProperty(0)
+
+    def bounce_ball(self, ball):
+        # manage the ball when it touch the paddle
+        if self.collide_widget(ball):
+            speedup = 1.1
+
+            # <- <- <-  ·  -> -> ->  --- ball's offset
+            # ______________________ --- ball paddle
+            offset = 0.02 * Vector(0, ball.center_y - self.center_y)
+            ball.velocity = speedup * (offset - ball.velocity)
+
+
 class PongBall(Widget):
     # velocity (speed) of the ball on x and y axis
     velocity_x = NumericProperty(0)
@@ -23,27 +38,43 @@ class PongBall(Widget):
 
 class PongGame(Widget):
     ball = ObjectProperty(None)
+    player1 = ObjectProperty(None)
+    player2 = ObjectProperty(None)
 
-    def serve_ball(self):
+    def serve_ball(self, vel=(4, 0)):
         self.ball.center = self.center
-        self.ball.velocity = Vector(4, 0).rotate(randint(0, 360))
+        self.ball.velocity = vel
 
     def update(self, dt):
         self.ball.move()
+
+        # bounce the paddles
+        self.player1.bounce_ball(self.ball)
+        self.player2.bounce_ball(self.ball)
 
         # bounce off top and bottom
         if (self.ball.y < 0) or (self.ball.top > self.height):
             self.ball.velocity_y *= -1
 
         # bounce off left and right
-        if (self.ball.x < 0) or (self.ball.right > self.width):
-            self.ball.velocity_x *= -1
+        if self.ball.x < self.x:
+            self.player2.score += 1
+            self.serve_ball(vel=(4, 0))
+        if self.ball.right > self.width:
+            self.player1.score += 1
+            self.serve_ball(vel=(-4, 0))
+
+    def on_touch_move(self, touch):
+        if touch.x < self.width / 3:
+            self.player1.center_y = touch.y
+        if touch.x > self.width * 2 / 3:
+            self.player2.center_y = touch.y
 
 
 class PongApp(App):
     def build(self):
         game = PongGame()
-        game.serve_ball()
+        game.serve_ball(vel=Vector(4 * (-1 if randint(-1, 1) >= 0 else 1), 0))
         Clock.schedule_interval(game.update, 1.0 / 60.0)
         return game
 

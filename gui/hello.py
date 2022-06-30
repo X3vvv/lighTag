775af999b319bbac41b4bd86adb8e855d583cb82
Zewen PAN <65343237+X3vvv@users.noreply.github.com>
2022-06-30 18:04:44 +0800

    adding functions to calculator. TODO: add()

--- gui/hello.py
+++ gui/hello.py
@@ -2,15 +2,33 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.lang import Builder
 from kivy.core.window import Window
-from kivy.uix.floatlayout import FloatLayout
+
+# set the window size
+Window.size = (500, 700)
 
 # explicitly assign the kv design file with random name
 Builder.load_file("hello.kv")
 
 
 class MyLayout(Widget):
-    def press(self, content):
-        print(content)
+
+    current_action = ""
+
+    def clear(self):
+        self.ids.calcInput.text = "0"
+
+    def button_press(self, button):
+        content = self.ids.calcInput.text
+
+        if content == "0":
+            self.ids.calcInput.text = button
+        else:
+            self.ids.calcInput.text = content + button
+
+    def add(self):
+        content = self.ids.calcInput.text
+
+        self.ids.calcInput.text += "+"
 
 
 class AwesomeApp(App):

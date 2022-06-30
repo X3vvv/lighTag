b9c4b88ad99e5b5e7a3941f376423abfe1cfda16
Zewen PAN <65343237+X3vvv@users.noreply.github.com>
2022-06-30 13:39:15 +0800

    practice making a calculator
    finish UI-beta

--- gui/hello.py
+++ gui/hello.py
@@ -2,21 +2,15 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.lang import Builder
 from kivy.core.window import Window
+from kivy.uix.floatlayout import FloatLayout
 
 # explicitly assign the kv design file with random name
 Builder.load_file("hello.kv")
 
 
 class MyLayout(Widget):
-    def press(self):
-        # create variables for the widget
-        name = self.ids.nameInput.text
-        # print(name)
-
-        # update the label
-        self.ids.nameLabel.text = f"Hello {name}!"
-        self.ids.nameInput.text = ""
-        self.ids.submitButton.text = "Quit"
+    def press(self, content):
+        print(content)
 
 
 class AwesomeApp(App):

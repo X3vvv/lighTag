eed26d5a683964ddd2fb2af3a66e6126dfac20d4
Zewen PAN <65343237+X3vvv@users.noreply.github.com>
2022-06-30 12:30:06 +0800

    change window bg color (method 2)
    by changing python file (instead of kv design file in method 1), but
    this can be overwritten by the settings in the kv design file.

--- gui/hello.py
+++ gui/hello.py
@@ -2,6 +2,8 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.lang import Builder
 
+from kivy.core.window import Window
+
 # explicitly assign the kv design file with random name
 Builder.load_file("hello.kv")
 
@@ -12,6 +14,12 @@ class MyLayout(Widget):
 
 class AwesomeApp(App):
     def build(self):
+        Window.clearcolor = (
+            0.2,
+            0.1,
+            0.4,
+            1,
+        )  # can be overwrite by kv design file's setting
         return MyLayout()
 
 

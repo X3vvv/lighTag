cb48bd804069fa53674ccad9a87ddeee23c780ac
Zewen PAN <65343237+X3vvv@users.noreply.github.com>
2022-07-03 11:59:39 +0800

    slightly optimize the layout appearance

--- gui/lighTag/main.py
+++ gui/lighTag/main.py
@@ -1,13 +1,27 @@
-from kivy.app import App
-from kivy.uix.widget import Widget
+from kivy import Config
+
+# set default window size and minumum size
+Config.set("graphics", "width", "450")
+Config.set("graphics", "height", "750")
+Config.set("graphics", "minimum_width", "450")
+Config.set("graphics", "minimum_height", "750")
+
 from kivy.lang import Builder
 
-
+# load kivy design file
 Builder.load_file("main.kv")
+
+from kivy.app import App
+from kivy.uix.widget import Widget
 
 
 class MainLayout(Widget):
-    pass
+    def settings_on(self):
+        self.ids.settings_img.source = "imgs/settings-outline-pressed.png"
+        # self.ids.settings_btn.background_color = (0, 0, 0, 0)
+
+    def settings_off(self):
+        self.ids.settings_img.source = "imgs/settings-outline.png"
 
 
 class MainApp(App):

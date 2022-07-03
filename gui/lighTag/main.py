421334f044ca709886cc1f67b12b533a176d0844
Zewen PAN <65343237+X3vvv@users.noreply.github.com>
2022-07-03 14:52:56 +0800

    add canvas size label, try to change base button position
    - canvas size is shown dynamically on the upper right corner of the canvas
    - clean up the structure
    - trying to dynamically change base button position after click confirm button

--- gui/lighTag/main.py
+++ gui/lighTag/main.py
@@ -1,4 +1,3 @@
-from tkinter import N
 from kivy import Config
 
 # set default window size and minumum size
@@ -7,12 +6,8 @@ Config.set("graphics", "height", "750")
 Config.set("graphics", "minimum_width", "450")
 Config.set("graphics", "minimum_height", "750")
 
-from kivy.lang import Builder
-
-# load kivy design file
-Builder.load_file("main.kv")
-
 from kivy.app import App
+from kivy.core.window import Window
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
@@ -21,9 +16,15 @@ from kivy.uix.popup import Popup
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.gridlayout import GridLayout
 
+ONE_CENTIMETER = 1  # 1 in kivy size unit
+
 
 class MainLayout(Widget):
     num_of_base = 0
+    focused_base = None
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
 
     def settings_on(self):
         self.ids.settings_img.source = "imgs/settings-outline-pressed.png"
@@ -34,26 +35,38 @@ class MainLayout(Widget):
 
     def add_base(self):
         base_id = self.num_of_base + 1
-        new_base = self._create_base(base_id)
+        new_base = Button(
+            text=str(base_id),
+            font_size=13,
+            size_hint=(None, None),
+            size=(17, 17),
+            pos_hint={"x": 0.5, "y": 0.5},
+            on_release=self._on_base_released,
+        )
         canvas = self.ids.canvas
         canvas.add_widget(new_base)
         self.num_of_base += 1
 
-    def _create_base_popup(self):
-        def popup_confirm(not_used):
-            # print(not_used)  # the button
-            x = base_x.text
-            y = base_y.text
-            z = base_z.text
+    def _on_base_released(self, base_btn):
+        self.ids.canvas.add_widget(self._create_base_popup(base_btn))
+        print(base_btn)
+
+    def _create_base_popup(self, base_btn):
+        def popup_confirm(confirm_btn):
+            # print(confirm_btn)
+            x = float(base_x.text)
+            y = float(base_y.text)
+            z = float(base_z.text)
             print(x, y, z)
+            base_btn.pos = [x, y]
             self.ids.canvas.remove_widget(popup)
 
         mainLayout = BoxLayout(orientation="vertical")
 
         posLayout = GridLayout(cols=2)
-        base_x = TextInput(multiline=False)
-        base_y = TextInput(multiline=False)
-        base_z = TextInput(multiline=False)
+        base_x = TextInput(multiline=False, text="0")
+        base_y = TextInput(multiline=False, text="0")
+        base_z = TextInput(multiline=False, text="0")
         posLayout.add_widget(Label(text="x:"))
         posLayout.add_widget(base_x)
         posLayout.add_widget(Label(text="y:"))
@@ -73,19 +86,6 @@ class MainLayout(Widget):
         )
 
         return popup
-
-    def _base_add_popup(self, not_used):
-        # print(not_used)  # the button object
-        self.ids.canvas.add_widget(self._create_base_popup())
-
-    def _create_base(self, id):
-        return Button(
-            text=str(id),
-            size_hint=(None, None),
-            size=(10, 10),
-            pos_hint={"x": 0.5, "y": 0.5},
-            on_release=self._base_add_popup,
-        )
 
 
 class MainApp(App):

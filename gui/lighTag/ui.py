8706e49c31963c293388dbd2643de3173dd37bc7
Zewen PAN <65343237+X3vvv@users.noreply.github.com>
2022-07-04 17:24:35 +0800

    Update ui.py

--- gui/lighTag/ui.py
+++ gui/lighTag/ui.py
@@ -34,21 +34,21 @@ from kivy.graphics import Line, Color
 
 # print("Socket connected.")
 
-ser = serial.Serial("/dev/cu.usbserial-110", 115200)    # 打开COM17，将波特率配置为115200，其余参数使用默认值
-if ser.isOpen():                        # 判断串口是否成功打开
+ser = serial.Serial("/dev/cu.usbserial-110", 115200)  # 打开COM17，将波特率配置为115200，其余参数使用默认值
+if ser.isOpen():  # 判断串口是否成功打开
     print("打开串口成功。")
-    print(ser.name)    # 输出串口号
+    print(ser.name)  # 输出串口号
 else:
     print("打开串口失败。")
 
-ser = serial.Serial(port="/dev/cu.usbserial-110",
-                    baudrate=115200,
-                    bytesize=serial.SEVENBITS,
-                    parity=serial.PARITY_NONE,
-                    stopbits=serial.STOPBITS_TWO,
-                    timeout=0.5) 
-
-
+ser = serial.Serial(
+    port="/dev/cu.usbserial-110",
+    baudrate=115200,
+    bytesize=serial.SEVENBITS,
+    parity=serial.PARITY_NONE,
+    stopbits=serial.STOPBITS_TWO,
+    timeout=0.5,
+)
 
 
 def iot_callback(duration_after_last_call):
@@ -237,6 +237,7 @@ class MainLayout(Widget):
         self.ids.canvas.add_widget(popup)
 
     def debug(self):
+        global inDisArr, tri
         for i in range(len(self.bases)):
             print("base[{}]: {}]".format(i, self.bases[i].pos))
         if len(self.bases) <= 0:
